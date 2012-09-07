a3cf0776116637b7e59a8634dc8d018fd4e92749
Guille <guillermo@artvisual.net>
2012-09-07 16:44:00 +0200

    prueba con DP

--- Resources/ui/styles.js
+++ Resources/ui/styles.js
@@ -3,12 +3,12 @@ module.exports = {
 	
 	header: {
 		backgroundColor: '#0000FF',
-		height:40,
+		height:'40 dp',
 		top:0
 	},
 	headerTitle: {
 		color:'#FFF',
-		font:{fontSize:18, fontWeight:'bold'}
+		font:{fontSize:'18 dp', fontWeight:'bold'}
 	}
 	
 }
