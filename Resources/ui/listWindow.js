8662467facc00205799206060e0f07764fa21dd4
Guille <guillermo@artvisual.net>
2012-09-10 18:28:55 +0200

    a

--- Resources/ui/listWindow.js
+++ Resources/ui/listWindow.js
@@ -1,20 +1,62 @@
 
 var Mods = require('/pathMods');
 
+if (Ti.Platform.osname === 'android') {
+	var $$ = require(Mods.styles_android);
+} else {
+	var $$ = require(Mods.styles_ios);
+}
+
 module.exports = function() {
 	
 	var MyWindow = require(Mods.configWindow);
 	
-	setTimeout(function() {
-		MyWindow().open();
-	}, 10);
+	if (!Ti.App.Properties.getDouble('date', null)) {
+		setTimeout(function() {
+			MyWindow().open();
+		}, 10);
+	}
 	
 	var win = Ti.UI.createWindow({
 		backgroundColor:'#F2F2F2',
 		exitOnClose:true
 	});
 	
-	win.add(Ti.UI.createLabel({text:'sdfja kdsjalsd jflñaks jflñasf lñsajlfñjasdklfjalñfdsj klñsajflña'}))
+	if (Ti.Platform.osname === 'android') {
+		var header = Ti.UI.createView($$.header);
+		var title = Ti.UI.createLabel($$.headerTitle);
+		title.text = L('main_title', 'Lagravidanza.net');
+		
+		header.add(title);
+		win.add(header);
+	} else {
+		win.title = L('main_title', 'Lagravidanza.net');
+		var nav = Ti.UI.iPhone.createNavigationGroup({
+			
+		});
+		win.add(nav);
+	}
+	
+	var todayButton = Ti.UI.createButton({
+		title:L('today', 'Hoy'),
+		right:'10 dp',
+		height:'20 dp',
+		width:'40 dp',
+		borderRadius:5,
+		borderColor:'#1A6A8A',
+		borderWidth:1,
+		color:'#FFF',
+		font:{fontWeigh:'bold'},
+		backgroundColor:'#1280AB'
+	});
+	
+	header.add(todayButton);
+	
+	var tableView = Ti.UI.createTableView({
+		
+		
+		
+	});
 	
 	return win;
 	
