baa4a4982bfb138d1ad3b27e441d0dbe67f6915e
Guille <guillermo@artvisual.net>
2012-11-26 15:56:37 +0100

    .

--- Resources/ui/rows.js
+++ Resources/ui/rows.js
@@ -47,7 +47,7 @@ module.exports = function(fullData, data, tableView, tableViewData, win) {
 		
 		if (Ti.Platform.osname === 'android') {
 			
-			data[i].header = 'no tiene header';
+			//data[i].header = 'no tiene header';
 			
 			var row = Ti.UI.createTableViewRow($$.row);
 			row.data = data[i];
@@ -157,7 +157,7 @@ module.exports = function(fullData, data, tableView, tableViewData, win) {
 				miniRow.add(image);
 			}
 			
-			data[i].header = 'no tiene header';
+			//data[i].header = 'no tiene header';
 			
 			if (data[i].header) {
 				
