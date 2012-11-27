216abdee6f574ea1920788bbb07ed2557ca05d71
Guille <guillermo@artvisual.net>
2012-11-27 09:56:07 +0100

    .

--- Resources/ui/listWindow.js
+++ Resources/ui/listWindow.js
@@ -257,6 +257,41 @@ module.exports = function(type) {
 		
 	}
 	
+	// require AdMob
+	var Admob = require('ti.admob');
+	
+	// then create an adMob view
+	var adMobView = Admob.createView({
+	    publisherId:"UA-36611802-1",
+	    testing:false, // default is false
+	    //top: 10, //optional
+	    //left: 0, // optional
+	    //right: 0, // optional
+	    bottom: 0, // optional
+	    adBackgroundColor:"FF8855", // optional
+	    backgroundColorTop: "738000", //optional - Gradient background color at top
+	    borderColor: "#000000", // optional - Border color
+	    textColor: "#000000", // optional - Text color
+	    urlColor: "#00FF00", // optional - URL color
+	    linkColor: "#0000FF" //optional -  Link text color
+	    //primaryTextColor: "blue", // deprecated -- now maps to textColor
+	    //secondaryTextColor: "green" // deprecated -- now maps to linkColor
+	    
+	});
+	
+	
+	//listener for adReceived
+	adMobView.addEventListener(Admob.AD_RECEIVED,function(){
+	   // alert("ad received");
+	   Ti.API.info("ad received");
+	});
+	
+	//listener for adNotReceived
+	adMobView.addEventListener(Admob.AD_NOT_RECEIVED,function(){
+	    //alert("ad not received");
+	     Ti.API.info("ad not received");
+	});
+	
 	return win;
 	
 }
