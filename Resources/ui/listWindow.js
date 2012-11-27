0844860db45f82e097a2fe580e956e86fe0ee90d
Guillermo <guillermo@artvisual.net>
2012-11-27 12:17:50 +0100

    .

--- Resources/ui/listWindow.js
+++ Resources/ui/listWindow.js
@@ -262,47 +262,87 @@ module.exports = function(type) {
 	// require AdMob
 	var Admob = require('ti.admob');
 	
-	// then create an adMob view
-	var adMobView = Admob.createView({
-	    publisherId:"a150b48b3d51124",
-	    testing:false, // default is false
-	    //top: 10, //optional
-	    //left: 0, // optional
-	    //right: 0, // optional
-	    bottom: '65dp', // optional
-	    adBackgroundColor:"FF8855", // optional
-	    backgroundColorTop: "738000", //optional - Gradient background color at top
-	    borderColor: "#000000", // optional - Border color
-	    textColor: "#000000", // optional - Text color
-	    urlColor: "#00FF00", // optional - URL color
-	    linkColor: "#0000FF" //optional -  Link text color
-	    //primaryTextColor: "blue", // deprecated -- now maps to textColor
-	    //secondaryTextColor: "green" // deprecated -- now maps to linkColor
-	    
-	});
+	if (Ti.Platform.osname === 'android') {
 	
-	
-	//listener for adReceived
-	adMobView.addEventListener(Admob.AD_RECEIVED, function(){
-	   // alert("ad received");
-	   Ti.API.info("ad received");
-	   tableView.bottom = '120dp';
-	});
-	
-	//setTimeout(function() {
-	todayButton.addEventListener('click', function() {
-		adMobView.requestAd();
-	});
-	//}, 5000);
-	
-	//listener for adNotReceived
-	adMobView.addEventListener(Admob.AD_NOT_RECEIVED, function(){
-	    //alert("ad not received");
-		Ti.API.info("ad not received");
-		tableView.bottom = '65dp';
-	});
-	
-	win.add(adMobView);
+		// then create an adMob view
+		var adMobView = Admob.createView({
+		    publisherId:"a150b48b3d51124",
+		    testing:false, // default is false
+		    //top: 10, //optional
+		    //left: 0, // optional
+		    //right: 0, // optional
+		    bottom: '65dp', // optional
+		    adBackgroundColor:"FF8855", // optional
+		    backgroundColorTop: "738000", //optional - Gradient background color at top
+		    borderColor: "#000000", // optional - Border color
+		    textColor: "#000000", // optional - Text color
+		    urlColor: "#00FF00", // optional - URL color
+		    linkColor: "#0000FF" //optional -  Link text color
+		    //primaryTextColor: "blue", // deprecated -- now maps to textColor
+		    //secondaryTextColor: "green" // deprecated -- now maps to linkColor
+		    
+		});
+		
+		
+		//listener for adReceived
+		adMobView.addEventListener(Admob.AD_RECEIVED, function(){
+		   // alert("ad received");
+		   Ti.API.info("ad received");
+		   tableView.bottom = '120dp';
+		});
+		
+		//setTimeout(function() {
+		todayButton.addEventListener('click', function() {
+			adMobView.requestAd();
+		});
+		//}, 5000);
+		
+		//listener for adNotReceived
+		adMobView.addEventListener(Admob.AD_NOT_RECEIVED, function(){
+		    //alert("ad not received");
+			Ti.API.info("ad not received");
+			tableView.bottom = '65dp';
+		});
+		
+		win.add(adMobView);
+		
+	} else { // iOS
+		
+		var ad = Admob.createView({
+		    bottom: 65, left: 0,
+		    width: 320, height: 50,
+		    publisherId: 'a150b49ef173c47', // You can get your own at http: //www.admob.com/
+		    //adBackgroundColor: 'black',
+		    testing: false,
+		    //dateOfBirth: new Date(1985, 10, 1, 12, 1, 1),
+		    //gender: 'male',
+		    //location: COORDS,
+		    keywords: ''
+		});
+		ad.addEventListener('didReceiveAd', function() {
+		    //alert('Did receive ad!');
+		    tableView.animate({bottom: 115});
+		});
+		ad.addEventListener('didFailToReceiveAd', function() {
+		    //alert('Failed to receive ad!');
+		    tableView.animate({bottom: 65});
+		});
+		ad.addEventListener('willPresentScreen', function() {
+		    //alert('Presenting screen!');
+		});
+		ad.addEventListener('willDismissScreen', function() {
+		    //alert('Dismissing screen!');
+		});
+		ad.addEventListener('didDismissScreen', function() {
+		    //alert('Dismissed screen!');
+		});
+		ad.addEventListener('willLeaveApplication', function() {
+		    //alert('Leaving the app!');
+		});
+		
+		win.add(ad);
+		
+	}
 	
 	return win;
 	
